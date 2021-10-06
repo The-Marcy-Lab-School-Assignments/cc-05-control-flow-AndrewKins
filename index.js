44d8a53cd2cc02c15fb995764ed5b73b888c4ea8
Andrew Enoe <andrewm.enoe@gmail.com>
2021-10-06 15:01:00 +0000

    Finished a few questions

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //1
-function absoluteValue(){
+function absoluteValue(num){
   
-}
+  if(typeof num === "number") {
+    if (num < 0 ) {
+        return num * -1;
+        } else if (num > 0) {
+     return(num);
+        } 
+    }  else {
+        return null;
+    }
+} 
+console.log(absoluteValue("kfoesfkes"))
 
 
 
